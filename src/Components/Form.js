8dc818d03096c72e46da02bf3e729b34a5401c26
mattvidakis <88048336+mattvidakis@users.noreply.github.com>
2022-03-22 15:58:17 -0400

    add validation

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,29 +2,89 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import formJSON from '../formData.json';
 import './Form.css'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 import bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MaskedInput from 'react-text-mask';
+import MaskedFormControl from 'react-bootstrap-maskedinput';
 
 const FormComponent = () => {
 
     const navigate = useNavigate();
-
+    const [current, setCurrent] = useState(null);
+    const [validated, setValidated] = useState(false);
+    const [toggleSubmit, setToggleSubmit] = useState(false);
     const [formData, setFormData] = useState({
         first_name: '', last_name: '', email: '', phone_number: '', street_address: '', post_code: '', country: ''
-    })
+    });
+    const [formErrors, setFormErrors] = useState({
+        first_name: '', last_name: '', email: '', phone_number: '', street_address: '', post_code: '', country: ''
+    });
+
+    useEffect(() => {
+        setFormErrors(validateForm(formData));
+    }, [formData])
 
     const onSubmit = (e) => {
-        const url = 'https://enovode7uq1r.x.pipedream.net/'
-        e.preventDefault();
-        const formDataJSON = JSON.stringify({
-            ...formData, phone_number: formatPhoneNumber(formData.phone_number)
-        });
-        localStorage.setItem('formData', formDataJSON);
-        axios.post(url, formData)
-        navigate(`/merci/${formData.first_name}`)
+        // const url = 'https://enovode7uq1r.x.pipedream.net/';
+        const form = e.currentTarget;
+        setToggleSubmit(true);
+        if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+            // setFormErrors(validateForm(formData));
+            // console.log(formErrors);
+
+        } else {
+            e.preventDefault();
+            setValidated(true);
+            const formDataJSON = JSON.stringify({
+                ...formData, phone_number: formatPhoneNumber(formData.phone_number)
+            });
+
+            localStorage.setItem('formData', formDataJSON);
+            // axios.post(url, formDataJSON);
+            navigate(`/merci/${formData.first_name}`);
+        }
     }
+
+    const validateForm = (values) => {
+        const errors = {};
+        const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        const phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+        if (!emailRegex.test(values.email)) {
+            errors.email = "SVP entrez un email valide"
+        } else { errors.email = "" }
+
+        if (!phoneRegex.test(values.phone_number)) {
+            errors.phone_number = "SVP entrez votre téléphone en format (123) 123-1234"
+        } else { errors.phone_number = "" }
+
+        if (!values.first_name) {
+            errors.first_name = 'Prénom non valide';
+        } else { errors.first_name = '' }
+
+        if (!values.last_name) {
+            errors.last_name = 'Nom nom valide'
+        } else { errors.last_name = '' }
+
+        if (!values.street_address) {
+            errors.street_address = 'SVP entrez un adresse'
+        } else { errors.street_address = '' }
+
+        if (!values.post_code) {
+            errors.post_code = 'SVP entrez un code postal'
+        } else { errors.post_code = '' }
+
+        if (!values.country) {
+            errors.country = 'SVP choisez un pays'
+        } else { errors.country = '' }
+        return errors;
+    }
+
+
 
     const formatPhoneNumber = (phoneNumberString) => {
         let cleaned = ('' + phoneNumberString).replace(/\D/g, '');
@@ -35,9 +95,22 @@ const FormComponent = () => {
         return null;
     }
 
+    const onChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+        // validateInput(e.target);
+        setCurrent(e.target)
+        // console.log(e.target.value)
+        console.log(formErrors)
+    }
+
     return (
         <div id="main-container" className="container pt-2 w-50 d-grid h-100">
-            <Form id="form" className="w-100" onSubmit={onSubmit}>
+            <Form noValidate validated={validated} id="form" className="w-100" onSubmit={onSubmit}>
+                <MaskedInput
+                    className="mb-3" id="input"
+                    mask={['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+
+                />
                 {
                     formJSON.questions.map((i) => {
                         return (
@@ -56,16 +129,28 @@ const FormComponent = () => {
                                     return (
                                         // map over fields
                                         v.map(fields => {
-
+                                            // console.log(fields)
                                             return (
                                                 Object.entries(fields).map(([k, v]) => {
+                                                    console.log(k, v)
                                                     if (v === 'text') {
+                                                        // do fields IF check here
                                                         return (
-                                                            <Form.Group key={fields.name} className="mb-3">
+                                                            <Form.Group key={fields.name} className="mb-3" controlId="input">
                                                                 <Form.Label className="mb-1">{fields.label}</Form.Label>
                                                                 <Form.Control
-                                                                    onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}
-                                                                    id="input" name={fields.name} type={fields.type} placeholder={fields.label} />
+                                                                    required
+                                                                    onChange={onChange}
+                                                                    onClick={e => console.log(formErrors)}
+                                                                    isValid={toggleSubmit && formData[fields.name]}
+                                                                    // isInvalid={toggleSubmit && !formData[fields.name].length}
+                                                                    isInvalid={toggleSubmit && formErrors[fields.name]}
+                                                                    value={formData[fields.name]}
+
+                                                                    name={fields.name} type={fields.type} placeholder={fields.label} />
+                                                                <Form.Control.Feedback type="invalid">
+                                                                    {formErrors[fields.name]}
+                                                                </Form.Control.Feedback>
                                                             </Form.Group>
                                                         )
                                                     } else if (v === 'dropdown') {
@@ -73,6 +158,8 @@ const FormComponent = () => {
                                                             <>
                                                                 <Form.Label key={fields.name} className="mb-1">{fields.label}</Form.Label>
                                                                 <Form.Select id="input" className="mb-3" aria-label={fields.label}
+                                                                    isInvalid={toggleSubmit && formErrors[fields.name]}
+                                                                    isValid={toggleSubmit && formData[fields.name]}
                                                                     name={fields.name} onChange={(e) => setFormData({ ...formData, [e.target.name]: e.target.value })}
 
                                                                 >
@@ -86,6 +173,9 @@ const FormComponent = () => {
                                                                         )
                                                                     })}
                                                                 </Form.Select>
+                                                                <Form.Control.Feedback type="invalid">
+                                                                    {formErrors[fields.name]}
+                                                                </Form.Control.Feedback>
                                                             </>
                                                         )
                                                     }
